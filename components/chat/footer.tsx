9dbb6c58aefbd30876248af0272e8c05e641907e
Son Nguyen <hoangson091104@gmail.com>
2025-04-20 20:56:46 -0400

    Final: AI Bot Working

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -3,24 +3,27 @@ import Link from "next/link";
 
 export default function ChatFooter() {
   return (
-    <div className="w-full text-xs flex pt-2 text-gray-500">
-      <div className="flex-grow text-left">
-        {/* Left Pane */}
+    <div className="w-full text-xs flex flex-wrap pt-2 text-gray-500">
+      {/* Left Pane */}
+      <div className="w-full sm:flex-1 text-center sm:text-left mb-1 sm:mb-0">
         <Link href="/terms" className="hover:underline">
           Terms of Service
         </Link>
       </div>
-      <div className="flex-grow text-center">
-        {/* Center Pane */}
+
+      {/* Center Pane */}
+      <div className="w-full sm:flex-1 text-center mb-1 sm:mb-0">
         {FOOTER_MESSAGE}
       </div>
-      <div className="flex-grow text-right">
-        {/* Right Pane */}
+
+      {/* Right Pane */}
+      <div className="w-full sm:flex-1 text-center sm:text-right">
         {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
         <a
           href="https://sonnguyenhoang.com"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:underline"
         >
           Developed by Son Nguyen in 2025
         </a>
